daf1dfdcdc9678953d114d21f9bfe142d6e54628
YacineYldrm <yacine@mailbox.org>
2023-11-09 09:25:26 +0100

    refactored code

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,19 +245,6 @@ console.log(sortedArray);
 
 console.log("%c leve 2_3 und 2_4", "color: blue; background-color: gold");
 
-// document.body.innerHTML = `<h1>Singers</h1> <div id="table-container"></div>`;
-
-// const singers2 = [
-//     { name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" }, 
-//     { name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
-// 	{ name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B"},
-// 	{ name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock"},
-// 	{ name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica"},
-// 	{ name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock"},
-// 	{ name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop"},
-// 	{ name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock"},
-// ];
-
 const tableContainer = document.body.querySelector("#table-container");
 const rowTitleContainer = document.body.querySelector("#row-title-container");
 tableContainer.innerHTML = '<table style="width: 70%" id="tableOutput"></table>'
